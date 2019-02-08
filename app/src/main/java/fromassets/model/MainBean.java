bde7c5f9eca0950868f81bbe91115225d6c804c3
ronald benjamin <dodo.ben.rb@gmail.com>
2019-02-08 10:43:45 +0530

    Initial commit

---
+++ app/src/main/java/fromassets/model/MainBean.java
@@ -0,0 +1,42 @@
+package fromassets.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 1033826 on 4/26/2017.
+ */
+
+public class MainBean {
+
+    //region dataobjects
+    @SerializedName("dataList")
+    @Expose
+    private ArrayList<DataList> dataList;
+    //endregion
+
+
+
+    //region getters_setters
+    public ArrayList<DataList> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(ArrayList<DataList> dataList) {
+        this.dataList = dataList;
+    }
+
+
+    //endregion
+
+
+
+
+
+
+
+
+
+}
